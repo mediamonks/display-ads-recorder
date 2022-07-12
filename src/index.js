89730f0585b8f01b2b1a4b538110cca0a95c08df
Mirko <mirko@mediamonks.com>
2022-07-12 09:48:23 +0200

    adds returned result from function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 module.exports = async function displayAdsRecorder(options) {
   const { targetDir, adSelection } = options;
+  let recordResult = {};
 
   for (const ad of adSelection.location) {
 
@@ -15,17 +16,23 @@ module.exports = async function displayAdsRecorder(options) {
     // convert screenshots to video and place in targetDir
     const outputPathVideo = path.join(targetDir, `${path.basename(path.dirname(ad))}.mp4`)
     const videoFile = await renderVideo(screenshots.baseDir, adSelection.fps, outputPathVideo);
+    recordResult.videoPath = outputPathVideo;
 
     // if gif is selected, convert video to GIF file and place in targetDir
     if (adSelection.output.indexOf('gif') !== -1) {
       const outputPathGif = path.join(targetDir, `${path.basename(path.dirname(ad))}.gif`)
       await renderGIf(path.resolve(videoFile), outputPathGif, adSelection.gifLoopOptions );
+      recordResult.gifPath = outputPathGif;
     }
+
 
     // if backup img is selected, convert last image from sequence and place in targetDir
     if (adSelection.output.indexOf('jpg') !== -1) {
       const outputPathImg = path.join(targetDir, `${path.basename(path.dirname(ad))}.jpg`)
       const backupImg = await getBackupImage(`${screenshots.baseDir}/${screenshots.files.slice(-1)}`, outputPathImg);
+      recordResult.jpgPath = outputPathImg;
     }
   }
+
+  return recordResult;
 }
