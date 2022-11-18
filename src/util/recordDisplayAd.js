b5b963495e44fae687fbe97001930cf5e5a7f861
Mirko <mirko@mediamonks.com>
2022-11-18 14:51:59 +0100

    switches to jpg screenshots

--- src/util/recordDisplayAd.js
+++ src/util/recordDisplayAd.js
@@ -11,12 +11,50 @@ const padLeadingZeros = (num, size) => {
   return s;
 };
 
+const minimal_args = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+];
+
 module.exports = async function recordDisplayAd(target, fps) {
   return new Promise(async (resolve, reject) => {
     // let rootPath = path.dirname(target);
     let screenshotBase = path.join(path.dirname(target), ".cache/screenshots/");
     let screenshotBaseFilename = "screenshot_";
-    const screenshotExt = "png";
+    const screenshotExt = "jpg";
     let screenshot_nr = 0;
     let nextFrame = 0;
     let adDetails = {};
@@ -37,6 +75,7 @@ module.exports = async function recordDisplayAd(target, fps) {
     const browser = await puppeteer.launch({
       //executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // if we ever want to include video. but then the video needs to be controlled by the timeline..
       headless: true, // headless to false for testing
+      args: minimal_args,
     });
 
     const page = await browser.newPage();
@@ -75,6 +114,8 @@ module.exports = async function recordDisplayAd(target, fps) {
           padLeadingZeros(screenshot_nr, 6) +
           "." +
           screenshotExt,
+        type: "jpeg",
+        quality: 100,
       });
 
       screenshot_nr++;
@@ -120,6 +161,8 @@ module.exports = async function recordDisplayAd(target, fps) {
     }
 
     await listenFor("message"); // Listen for "message" custom event on page load.
+
+    console.log("minimal args");
 
     await page.goto(url, {
       // waitUntil: "networkidle0",
--- src/util/renderVideoFromFiles.js
+++ src/util/renderVideoFromFiles.js
@@ -1,37 +1,43 @@
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
-const cliProgress = require('cli-progress');
+const cliProgress = require("cli-progress");
 
-module.exports = async function renderVideoFromFiles(filesPath, fps = 30, output = 'video.mp4') {
-  const screenshotExt = 'png';
-  const input = `${filesPath}/screenshot_%06d.${screenshotExt}`
+module.exports = async function renderVideoFromFiles(
+  filesPath,
+  fps = 30,
+  output = "video.mp4"
+) {
+  const screenshotExt = "jpg";
+  const input = `${filesPath}/screenshot_%06d.${screenshotExt}`;
 
-  const progressBar = new cliProgress.SingleBar({
-    format: 'rendering video          [{bar}] {percentage}%'
-  }, cliProgress.Presets.shades_classic);
+  const progressBar = new cliProgress.SingleBar(
+    {
+      format: "rendering video          [{bar}] {percentage}%",
+    },
+    cliProgress.Presets.shades_classic
+  );
   progressBar.start(100, 0);
 
   return new Promise((resolve, reject) => {
     try {
       const process = ffmpeg();
       process.addInput(input);
-      process.fpsInput(fps)
-      process.fps(fps)
-      process.videoBitrate(10000)
+      process.fpsInput(fps);
+      process.fps(fps);
+      process.videoBitrate(10000);
       process.output(output);
-      process.on('progress', (progress) => {
+      process.on("progress", (progress) => {
         progressBar.update(Math.round(progress.percentage));
-      })
-      process.on('end', () => {
+      });
+      process.on("end", () => {
         progressBar.stop();
-        resolve(output)
-      })
+        resolve(output);
+      });
 
       process.run();
-
     } catch (e) {
       reject(e);
     }
-  })
-}
+  });
+};
