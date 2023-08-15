23dd44546c2200079e346aeffb97fd61d2c0871f
Vasyl Ilba <vasyl.ilba@mediamonks.com>
2023-08-15 18:50:59 +0300

    fixes error on cli -g -1 throwing error. now uses words loop/once instead

--- cli/displayAdsRecorder.js
+++ cli/displayAdsRecorder.js
@@ -17,7 +17,7 @@ const findAdsInDirectory = require("../src/util/findAdsInDirectory");
     .option('-a, --all', 'If you want to record all', false)
     .option('-j, --jpg [size]', 'If you want to output jpg and optional kbs')
     .addOption(
-      new program.Option("-g, --gif [loop]", "If you want to output animated gifs and loop them or play once").choices(['-1', '0'])
+      new program.Option("-g, --gif [loop]", "If you want to output animated gifs and loop them or play once").choices(['once', 'loop'])
     )
     .option("-m, --mp4", "If you want to output video")
     .addOption(
@@ -47,9 +47,9 @@ const findAdsInDirectory = require("../src/util/findAdsInDirectory");
 
   console.log(`found ${allAds.length} ad(s)`);
   
-  const { location } = options.all
-  ? { location: allAds }
-  : await inquirer.prompt({
+  const location = options.all
+  ? allAds
+  : (await inquirer.prompt({
       type: "checkbox",
       name: "location",
       message: "Please select ad(s) to record:",
@@ -63,7 +63,7 @@ const findAdsInDirectory = require("../src/util/findAdsInDirectory");
           };
         }),
       ],
-    });
+    })).location;
 
   const outputChoices = [
     { name: "mp4", value: "mp4", checked: false },
@@ -71,20 +71,26 @@ const findAdsInDirectory = require("../src/util/findAdsInDirectory");
     { name: "jpg (last frame)", value: "jpg", checked: false },
   ]
 
-  const { output } = outputChoices.some(e => options[e.value] != undefined)
-  ? { output: outputChoices.map(e => e.value).filter(e => options[e] != undefined) }
-  : await inquirer.prompt({
+  const output = outputChoices.some(e => options[e.value] != undefined)
+  ? outputChoices.map(e => e.value).filter(e => options[e] != undefined)
+  : (await inquirer.prompt({
       type: "checkbox",
       name: "output",
       message: "Please select output(s)",
       validate: (answers) => answers.length > 0,
       choices: outputChoices
-    });
+    })).output;
 
-  const { gifLoopOptions } = output.includes("gif")
+
+  const gifLoopOptionsMap = {
+    'once': -1,
+    'loop': 0
+  }
+
+  const gifLoopOptions = output.includes("gif")
   ? options.gif != true
-    ? { gifLoopOptions: options.gif }
-    : await inquirer.prompt({
+    ? gifLoopOptionsMap[options.gif]
+    : (await inquirer.prompt({
         type: "list",
         name: "gifLoopOptions",
         message:
@@ -94,31 +100,31 @@ const findAdsInDirectory = require("../src/util/findAdsInDirectory");
           { name: "Loop", value: "0" },
         ],
         default: 0,
-      })
-  : { gifLoopOptions: undefined }
+      })).gifLoopOptions
+  : undefined
 
-  const { fps } = (output.includes("mp4") || output.includes("gif"))
+  const fps = (output.includes("mp4") || output.includes("gif"))
   ? options.fps
-    ? options
-    : await inquirer.prompt({
+    ? options.fps
+    : (await inquirer.prompt({
         type: "list",
         name: "fps",
         message: "Please select fps to record at",
         choices: [{ name: 15 }, { name: 30 }, { name: 60 }],
         default: 1,
-      })
-  : { fps: undefined }
+      })).fps
+  : undefined
 
-  const { jpgMaxFileSize } = output.includes("jpg")
+  const jpgMaxFileSize = output.includes("jpg")
   ? !options.jpg || options.jpg == true // so if no flag at all or flag only
-    ? await inquirer.prompt({
+    ? (await inquirer.prompt({
         type: "input",
         name: "jpgMaxFileSize",
         message: "Please select max KB filesize for backup image",
         default: 40,
-      })
-    : { jpgMaxFileSize: options.jpg }
-  : { jpgMaxFileSize: undefined }
+      })).jpgMaxFileSize
+    : options.jpg
+  : undefined
 
   const adSelection = {
     output,
